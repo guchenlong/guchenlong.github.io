018e7104ece273c630d12c6b42d981bf559e20a3
2401_88814512 <1653227110@qq.com>
2025-06-08 21:06:18 +0800

    feat: add

--- common/js/base.js
+++ common/js/base.js
@@ -1,11 +1,13 @@
 // 百度统计
 (function() {
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?50c6c1ceac5a4df01e7db17eb06290a9";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-    })();
+    if (window.location.hostname === "guchenlong.github.io") {
+        // 仅线上生效
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?50c6c1ceac5a4df01e7db17eb06290a9";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+        })();
+    }
 })();
-
