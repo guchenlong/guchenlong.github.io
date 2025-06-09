2e17cfc071ed2a056d5bc8daf3d0a3d8a0ea42ae
2401_88814512 <1653227110@qq.com>
2025-06-09 21:53:07 +0800

    feat: 视频压缩

--- common/components/left_navigation/left_navigation.js
+++ common/components/left_navigation/left_navigation.js
@@ -5,6 +5,7 @@ function render_left_navigation() {  // 渲染 - 左侧导航栏
         { "title": "首页", "link": "/" },
         { "title": "图片压缩", "link": "/pages/compress/image/index.html" },
         { "title": "字体压缩", "link": "/pages/compress/font/index.html" },
+        { "title": "视频压缩", "link": "/pages/compress/video/index.html" },
         { "title": "JSON 格式化", "link": "/pages/format/json/index.html" },
     ];
 
