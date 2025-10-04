3d11e7aefb917c3aee6dab639df523c1e08bbc73
2401_88814512 <1653227110@qq.com>
2025-10-04 23:50:35 +0800

    feat: 日韩

--- common/js/base.js
+++ common/js/base.js
@@ -4,10 +4,11 @@ function init_baidu_statistics() {
     if (window.location.hostname === "guchenlong.github.io") {
         // 仅线上生效
         var _hmt = _hmt || [];
-        (function() {
+        (function () {
             var hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?50c6c1ceac5a4df01e7db17eb06290a9";
-            var s = document.getElementsByTagName("script")[0]; 
+            hm.src =
+                "https://hm.baidu.com/hm.js?50c6c1ceac5a4df01e7db17eb06290a9";
+            var s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(hm, s);
         })();
     }
