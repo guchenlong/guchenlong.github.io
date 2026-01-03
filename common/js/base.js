27880c5eeb5a2cd9171cdb860fa63a442122daa8
2401_88814512 <1653227110@qq.com>
2026-01-03 23:22:05 +0800

    首页

--- common/js/base.js
+++ common/js/base.js
@@ -4,11 +4,13 @@ window.onload = function () {
 
 // 初始化 百度统计
 function initBaiduStatistics() {
-    var _hmt = _hmt || [];
-    (function () {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
+    if (window.location.hostname === "guchenlong.github.io") {
+        var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    }
 }
