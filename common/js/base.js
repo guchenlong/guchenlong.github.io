0f01aceeb37559969c72dcdae228974d4774c96e
2401_88814512 <1653227110@qq.com>
2026-01-03 18:56:15 +0800

    调整 百度统计

--- common/js/base.js
+++ common/js/base.js
@@ -3,11 +3,18 @@ function init_baidu_statistics() {
     console.log("init baidu statistics");
     if (window.location.hostname === "guchenlong.github.io") {
         // 仅线上生效
+        // var _hmt = _hmt || [];
+        // (function () {
+        //     var hm = document.createElement("script");
+        //     hm.src =
+        //         "https://hm.baidu.com/hm.js?50c6c1ceac5a4df01e7db17eb06290a9";
+        //     var s = document.getElementsByTagName("script")[0];
+        //     s.parentNode.insertBefore(hm, s);
+        // })();
         var _hmt = _hmt || [];
         (function () {
             var hm = document.createElement("script");
-            hm.src =
-                "https://hm.baidu.com/hm.js?50c6c1ceac5a4df01e7db17eb06290a9";
+            hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
             var s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(hm, s);
         })();
