cf68afa54da813661a82e6a4c7f589dc98e711d2
2401_88814512 <1653227110@qq.com>
2026-01-05 23:23:23 +0800

    update

--- common/components/headerComponent.js
+++ common/components/headerComponent.js
@@ -6,6 +6,6 @@ function initHeaderComponent() {
     var header = document.getElementsByTagName("header")[0];
     header.innerHTML = "顾晨龙的个人站";
     header.onclick = function () {
-        window.location.href = "https://guchenlong.github.io/";
+        window.location.href = "http://guchenlong.com";
     }
 }
