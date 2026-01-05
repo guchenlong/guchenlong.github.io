9663d2f298cf3b0e962a0c3364086d6c3c6d3ab0
2401_88814512 <1653227110@qq.com>
2026-01-05 22:51:32 +0800

    update

--- common/js/base.js
+++ common/js/base.js
@@ -1,17 +1,8 @@
-window.onload = function () {
-    initBaiduStatistics(); // 初始化 百度统计
-    // initHeaderComponent(); // 初始化 头部 组件
-}
-
-// 初始化 百度统计
-function initBaiduStatistics() {
-    // if (window.location.hostname === "guchenlong.github.io") {
-    var _hmt = _hmt || [];
-    (function () {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-    // }
-}
+// 百度统计 上报
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
