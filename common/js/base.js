cfd0ca6a26ad73b32b5c702a8163039ee943c5ad
2401_88814512 <1653227110@qq.com>
2026-01-25 14:10:36 +0800

    update

--- common/js/base.js
+++
@@ -1,8 +0,0 @@
-// 百度统计 上报
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
---
+++ common/js/common.js
@@ -0,0 +1,15 @@
+// 百度统计 上报
+function initBaiduTongji() {
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+}
+
+// 跳转 页面
+function jumpTo(url) {
+    window.open(url);
+}
