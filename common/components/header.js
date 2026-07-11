a0f293cc1eefc0db6f7aa8ef050847ec616699cf
2401_88814512 <1653227110@qq.com>
2026-07-11 12:14:43 +0800

    add 沪教版 数学

--- common/components/header.js
+++ common/components/header.js
@@ -4,13 +4,7 @@ function renderHeaderComponent() {
     // 组件 - 元素
     element.innerHTML = '顾晨龙的个人站';
     // 组件 - 样式
-    element.style.width = '100%';
-    element.style.height = '50px';
-    element.style.borderBottom = '1px solid #ddd';
-    element.style.lineHeight = '50px';
-    element.style.textAlign = 'center';
-    element.style.fontSize = '18px';
-    element.style.cursor = 'pointer';
+    element.classList.add('website-name');
     // 组件 - 事件
     element.addEventListener('click', function () {
         window.open("/", "_blank");
@@ -24,12 +18,7 @@ function renderPageNameComponent(pageName) {
     // 组件 - 元素
     element.innerHTML = pageName;
     // 组件 - 样式
-    element.style.width = '100%';
-    element.style.height = '50px';
-    element.style.lineHeight = '50px';
-    element.style.textAlign = 'center';
-    element.style.fontSize = '16px';
-    element.style.borderBottom = '1px solid #ccc';
+    element.classList.add('page-name');
     // 组件 - 事件
     element.addEventListener('click', function () {
         // window.open("https://guchenlong.github.io", "_blank");
