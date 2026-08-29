425dc8042fda6b5117b814069829d156d3f90c63
2401_88814512 <1653227110@qq.com>
2026-08-30 00:50:59 +0800

    add 统计

---
+++ common/js/common.js
@@ -0,0 +1,10 @@
+// 1、埋点上报
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+console.log('common.js');
