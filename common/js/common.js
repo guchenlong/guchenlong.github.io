5648bad3f91d64964782acaa507cb9e473ac6970
2401_88814512 <1653227110@qq.com>
2026-08-30 10:53:42 +0800

    统计上报限制添加

--- common/js/common.js
+++ common/js/common.js
@@ -1,8 +1,10 @@
 // 1、埋点上报
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+if (window.location.host = "guchenlong.github.io") {
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?625b644748b71c4eec887e2a8ba80945";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+}
